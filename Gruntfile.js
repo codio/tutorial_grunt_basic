0efe9e7c4b78e0c36cfa7dbc96bb4e77b3834bfa
Freddy May <fmay@applicationcraft.com>
2014-01-06 17:23:35 +0000

    help

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,8 @@ module.exports = function(grunt) {
 
   // files to be minified and combined
   var cssFiles = [
-    'css/main.css',
-    'css/normalize.css'
+    'app/css/main.css',
+    'app/css/normalize.css'
   ];
 
   // this is where all the grunt configs will go
@@ -14,23 +14,22 @@ module.exports = function(grunt) {
 
     // configuration for the cssmin task
     // note that this syntax and options can found on npm page of any grunt plugin/task
-    cssmin: {
-      // options for css min task
-      options:{
-        // banner to be put on the top of the minified file using package name and todays date
-        // note that we are reading our project name using pkg.name i.e name of our project
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      combine: {
-        // options for combining files
-        // we have defined cssFiles variable to hold our file names at the top
+
+    uglify: {
+      my_target: {
         files: {
-          // here key part is output file which will our <package name>.min.css
-          // value part is set of input files which will be combined/minified
-          'css/<%= pkg.name %>.min.css': cssFiles
+          'dest/output.min.js': ['app/js/*.js']
         }
       }
-    }
+    }, 
+
+    cssmin: {
+      combine: {
+        files: {
+          'dest/css/styles.css': ['app/css/main.css', 'normailize.css']
+        }
+      }
+    }    
 
   }); // end of configuring the grunt task
 
@@ -38,8 +37,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
+  grunt.registerTask('cssmin', ['cssmin']);
+  // main build task
   grunt.registerTask('default', ['cssmin']);
-  // cssmin task
-  grunt.registerTask('buildcss', ['cssmin']);
 
 };
